14dbf3cb1266c26c86f46fe5101fc00f8b86b604
Vikram.Dhanapalan <vikram.the.coder@gmail.com>
2024-09-03 21:44:18 +0530

    Made editable fields optional

--- Contacts/Contact/forms.py
+++ Contacts/Contact/forms.py
@@ -5,3 +5,13 @@ class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
         fields = '__all__'
+
+class PersonForm2(forms.ModelForm):
+    name = forms.CharField(required=False)
+    dob = forms.DateField(required=False)
+    email = forms.EmailField(required=False)
+    mobile = forms.CharField(required=False)
+    dp_pic = forms.ImageField(required=False)
+    class Meta:
+        model = Person
+        fields = '__all__'
